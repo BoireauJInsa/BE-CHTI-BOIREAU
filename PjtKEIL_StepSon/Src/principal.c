b7986c079e45228681662f1a30bfd80b34c45669
BoireauJInsa <boireau-@insa-toulouse.fr>
2023-04-07 17:39:53 +0200

    obesrvation sortieson

---
+++ PjtKEIL_StepSon/Src/principal.c
@@ -0,0 +1,43 @@
+
+
+#include "DriverJeuLaser.h"
+
+void timer_callback(void);
+
+int main(void)
+{
+
+// ===========================================================================
+// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+// ===========================================================================
+
+// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+CLOCK_Configure();
+
+	
+Timer_1234_Init_ff(TIM4, 91 * 72); //91ms
+Active_IT_Debordement_Timer( TIM4, 2, timer_callback);
+	
+
+
+	
+	
+
+//============================================================================	
+	
+	
+while	(1)
+	{
+	}
+}
+
+/*
+static int position;
+
+void CallbackSon(void){
+	if (position < LongeurSon){
+		SortieSon = son[position] * (360 / 32767) + 360
+		position = position +1
+	}
+}
+*/
